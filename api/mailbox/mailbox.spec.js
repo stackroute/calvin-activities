5998e287334c08dd27c7e2e9b343fc27c6fba320
nithie <nithi.kbe@gmail.com>
2017-06-12 17:28:18 +0530

    Corrected Eslint errors and removed cartoons api

--- api/mailbox/mailbox.spec.js
+++ api/mailbox/mailbox.spec.js
@@ -6,7 +6,6 @@ describe('/mailbox api', () => {
   it('it should create a new mailbox', (done) => {
     server
       .post('/mailbox')
-     // .expect(201)
       .end((err, res) => {
         if (err) { done(err); return; }
         res.status.should.equal(201);
@@ -15,39 +14,35 @@ describe('/mailbox api', () => {
   });
 
   it('should return api mailbox', (done) => {
-    // calling home page api
     server
-    .get('/mailbox')
-    .expect('Content-type', /json/)
-    // .expect(200) // THis is HTTP response
-    .end((err, res) => {
-      if (err) { done(err); return; }
-      res.status.should.equal(200);
-      done();
-    });
+      .get('/mailbox')
+      .expect('Content-type', /json/)
+      .end((err, res) => {
+        if (err) { done(err); return; }
+        res.status.should.equal(200);
+        done();
+      });
   });
 
   it('should return a particular mailbox', (done) => {
-    // calling home page api
     server
-    .get('/mailbox/2')
-    .expect(200)
-    // .expect(200) // THis is HTTP response
-    .end((err, res) => {
-      if (err) { done(err); return; }
-      res.status.should.equal(200);
-      done();
-    });
+      .get('/mailbox/0')
+      .expect(200)
+      .end((err, res) => {
+        if (err) { done(err); return; }
+        res.status.should.equal(200);
+        done();
+      });
   });
 
   it('should delete a mailbox', (done) => {
     server
-    .delete('/mailbox/7')
-    .expect(200)
-    .end((err, res) => {
-      if (err) { done(err); return; }
-      res.status.should.equal(200);
-      done();
-    });
+      .delete('/mailbox/0')
+      .expect(200)
+      .end((err, res) => {
+        if (err) { done(err); return; }
+        res.status.should.equal(200);
+        done();
+      });
   });
 });
