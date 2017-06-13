f73f56d6ddbc20114972bbb6742bbe805d9d1176
revathynattamai <revathy.ng@gmail.com>
2017-06-14 03:51:48 +0530

    Completed mailbox API

--- api/mailbox/index.js
+++ api/mailbox/index.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 const controller = require('./mailbox.controller');
 
 router.post('/', controller.createMailbox);
-// router.get('/', controller.retrieveAllMail);
-// router.get('/:id', controller.retrieveMailbox);
 router.delete('/:id', controller.deleteMailbox);
 
 module.exports = router;
--- dao/mailbox/index.js
+++ dao/mailbox/index.js
@@ -1,45 +1,32 @@
-const mailbox = [];
+const mailboxes = [];
 let idCounter = -1;
 
-//Function to create a mailbox which contains id
+// Function to create a mailbox which contains id
 
 function createMailbox(req, res) {
-  const newmailbox = {
-    id: idCounter += 1,
+  const createNewMailBox = {
+    id: (idCounter += 1).toString(),
   };
-  mailbox.push(newmailbox);
-  return newmailbox;
+
+  mailboxes.push(createNewMailBox);
+  return createNewMailBox;
 }
 
-function checkIfMailboxExists(id){
-  let filteredMailbox =  mailbox.filter(userid => userid.id === id);
-  return mailbox.indexOf(filteredMailbox[0]);
+function checkIfMailboxExists(id) {
+  const filteruserid = mailboxes.filter(userid => userid.id === id);
+  return filteruserid.length!==0;
 }
-
-//Function to get the mailbox with particular id
-// function retrieveMailbox(id) {
-//   let mailBoxIndex = checkIfMailboxExists(id);
-//   if (mailBoxIndex === -1) {
-//     return null;
-//   }
-//   return mailbox[mailBoxIndex];
-// }
-
-//Function to delete the mailbox with id. If id not exists returns no mailbox error
+// Function to delete the mailbox with id. If id not exists returns no mailbox error
 function deleteMailbox(id) {
-  let mailBoxIndex = checkIfMailboxExists(id);
- 
-   if (mailBoxIndex === -1) {
-     return 0;
-   }
-  else {
-    const x = mailbox.splice(mailBoxIndex, 1);
-    return  x[0];
-  }
+  const filteruserid = mailboxes.filter(userid => userid.id === id);
+  const mailBoxIndex = mailboxes.indexOf(filteruserid[0]);
+  const x = mailboxes.splice(mailBoxIndex, 1);
+  return x[0];
 }
 
 module.exports = {
- checkIfMailboxExists,
- createMailbox,
- deleteMailbox,
+  createMailbox,
+  deleteMailbox,
+  checkIfMailboxExists,
 };
+
