34e53b8e98960653b1e31dcd537088222f212653
revathynattamai <revathy.ng@gmail.com>
2017-06-13 22:49:35 +0530

    Removed retrive api since it is not used

--- api/mailbox/index.js
+++ api/mailbox/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const controller = require('./mailbox.controller');
 
-router.post('/', controller.createNewMail);
-router.get('/', controller.retrieveAllMail);
-router.get('/:id', controller.retrievemail);
-router.delete('/:id', controller.deletemail);
+router.post('/', controller.createMailbox);
+// router.get('/', controller.retrieveAllMail);
+// router.get('/:id', controller.retrieveMailbox);
+router.delete('/:id', controller.deleteMailbox);
 
 module.exports = router;
