c74a8900f1f740c04a64289c423b91d72a7a3846
rajatchauhan126 <rajatchauhan126@gmail.com>
2017-06-16 01:11:40 +0530

    swagger api documentation with express

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(require('body-parser').json());
 
 app.use('/cartoons', require('./api/cartoons'));
