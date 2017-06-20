c40407b8693bd7c1fe4df20f6b69d0e237f8977e
Gayathri-gayu <gp124120@gmail.com>
2017-06-20 21:11:46 +0530

    documented publish Activity and receive notification using swagger

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-// require('./api/push');
-
 const express = require('express');
 
 const app = express();
-const server = require('http').Server(app);
 
-// const io = require('socket.io')(server);
+const swaggerUi = require('swagger-ui-express');
 
-// const socket = io.listen(server);
+const req = require('require-yml');
 
-// const time = require('express-timestamp');
-
-// app.use(time.init);
-
-// const swaggerUi = require('swagger-ui-express');
-
-// const req = require('require-yml');
-
-// const swaggerDocument = req('./swagger/api.yml');
+const swaggerDocument = req('./swagger/api.yml');
 
 // const swaggerDocumentMailbox = req('./swagger/apiMailbox.yml');
 
@@ -25,7 +14,7 @@ const server = require('http').Server(app);
 
 app.use(require('body-parser').json());
 
-// app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocumentMailbox));
 
@@ -36,17 +25,6 @@ app.use('/circle', require('./api/circle'));
 app.use('/mailbox', require('./api/mailbox'));
 
 /** Follow URI is for /mailbox/:mailboxId/circle/:circleId */
-app.use('/mailbox', require('./api/follow'));
-
-
-// module.exports = { server, socket };
-
-// push(socket);
-
-// module.exports = {server,socket};
-// CHANGEME: URI should be same as circle's. Follow the example of lines 11-12
-
-app.use('/circle', require('./api/activity'));
-
+app.use('/mailbox/', require('./api/follow'));
 
 module.exports = app;
