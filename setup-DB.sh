2a7eb33fb164aed297443a6a56ed448a87ef90e3
nithie <nithi.kbe@gmail.com>
2017-06-21 18:29:05 +0530

    modified setup-DB.sh file

--- setup-DB.sh
+++ setup-DB.sh
@@ -5,4 +5,4 @@ set -e
 cqlsh -e "create keyspace testdb with replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }"
 cqlsh -e "create table testdb.circle (id uuid primary key)"
 cqlsh -e "create table testdb.mailbox(id uuid primary key)"
-cqlsh -e "create table testdb.follow(id uuid primary key,circleId text,mailboxId text)"
+cqlsh -e "create table testdb.follow(circleId uuid,mailboxId uuid, primary key(circleid,mailboxid))"
