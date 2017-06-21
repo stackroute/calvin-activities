7e7928f8a374223d670ede1a99da985f2b32e22b
nithie <nithi.kbe@gmail.com>
2017-06-21 18:07:02 +0530

    Modified db modelling of follow api

--- dao/cassandra/follow/index.js
+++ dao/cassandra/follow/index.js
@@ -1,12 +1,10 @@
 const start=require('../../../db');
 
 const client=start.client;
-const uuid = start.uuid;
 
 function addFollow(follower, callback) {
-  const id1 = uuid();
-  const query = ('INSERT INTO follow (id, circleid, mailboxid ) values( ?, ?, ? )');
-  client.execute(query, [id1, follower.circleId, follower.mailboxId], (err, result) => {
+  const query = ('INSERT INTO follow (circleid, mailboxid ) values(?, ? )');
+  client.execute(query, [follower.circleId, follower.mailboxId], (err, result) => {
     if (err) { throw err; }
     return callback(err, follower);
   });
@@ -14,7 +12,7 @@ function addFollow(follower, callback) {
 
 function checkIfFollowExists(follower, callback) {
   const query = (`SELECT * from follow where circleid = ${follower.circleId}
-   AND mailboxid = ${follower.mailboxId} ALLOW FILTERING`);
+   AND mailboxid = ${follower.mailboxId}`);
   client.execute(query, (err, result) => {
     if (err) { return callback(err); }
     return callback(null, result.rowLength > 0);
@@ -22,15 +20,9 @@ function checkIfFollowExists(follower, callback) {
 }
 
 function deleteFollow(follower, callback) {
-  const query =(`SELECT * from follow where circleid =${follower.circleId} 
-    AND mailboxid=${follower.mailboxId} ALLOW FILTERING`);
-  client.execute(query, (error, result) => {
-    if (error) { return callback(error, null); }
-    const deleteQuery =(`DELETE FROM follow where id =${result.rows[0].id}`);
-    client.execute(deleteQuery, (err) => {
-      if (err) { return callback(err); }
-      return 0;
-    });
+  const deleteQuery =(`DELETE FROM follow where id =${follower.circleId} AND mailbox=${follower.mailboxId}`);
+  client.execute(deleteQuery, (err) => {
+    if (err) { return callback(err); }
     return callback(null, follower);
   });
 }
