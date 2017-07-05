f8606bc82c204a5f4e09ed1b015778054bff6808
ma301895 <mayank.sethi1@wipro.com>
2017-07-05 17:38:06 +0530

    fixed lint errors in l1route

--- l1route/index.js
+++ l1route/index.js
@@ -12,21 +12,21 @@ const redisClient = redis.createClient({
   database: 1,
 });
 
-  consumer.on('message', (message) => {
-    const key = `${config.l1Namespace}:${JSON.parse(message.value).circleID}`;
-    const msg = JSON.parse(message.value).message;
-    redisClient.info('server')(function (error, res) {
-      return this.select(0);
-    })(function (error, res) {
-      return this.smembers(key);
-    })(function (error, res) {
-      const payloads =[];
-      res.forEach((element) => {
-        payloads.push({ topic: element, messages: [JSON.stringify(msg)] });
-      }, this);
-      producer.send(payloads, (err, data) => {
-        console.log(data);
-      });
+consumer.on('message', (message) => {
+  const key = `${config.l1Namespace}:${JSON.parse(message.value).circleID}`;
+  const msg = JSON.parse(message.value).message;
+  redisClient.info('server')(function (error, res) {
+    return this.select(0);
+  })(function (error, res) {
+    return this.smembers(key);
+  })(function (error, res) {
+    const payloads =[];
+    res.forEach((element) => {
+      payloads.push({ topic: element, messages: [JSON.stringify(msg)] });
+    }, this);
+    producer.send(payloads, (err, data) => {
+      console.log(data);
     });
   });
+});
 
