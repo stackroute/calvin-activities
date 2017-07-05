acc6ea98f166eea9b266652d8189dbbc42facdd7
ma301895 <mayank.sethi1@wipro.com>
2017-07-05 17:23:29 +0530

    fixed lint errors

--- l1route/index.js
+++ l1route/index.js
@@ -6,33 +6,31 @@ const producer = kafka.producer;
 
 const consumer = kafka.consumer;
 
-const redis = require('thunk-redis')
+const redis = require('thunk-redis');
 
-const thunk = require('thunks')()
+const thunk = require('thunks')();
 
 const redisClient = redis.createClient({
-  database: 1
+  database: 1,
 });
 
-producer.on('ready', function () {
-    consumer.on('message', function (message) {
-        const key = config.l1Namespace+":"+JSON.parse(message.value).circleID;
-        const msg = JSON.parse(message.value).message;
-        redisClient.info('server')(function (error, res) {
-        return this.select(0)
-        })(function (error, res) {
-        return this.smembers(key)
-        })(function (error, res) {
-        let payloads =[];
-            res.forEach(function(element) {
-                payloads.push({ topic: element, messages: [ JSON.stringify(msg) ] });
-            }, this);
-            producer.send(payloads, function (err, data) {
-            console.log(data);
-            });
-        })
-    });   
+producer.on('ready', () => {
+  consumer.on('message', (message) => {
+    const key = `${config.l1Namespace}:${JSON.parse(message.value).circleID}`;
+    const msg = JSON.parse(message.value).message;
+    redisClient.info('server')(function (error, res) {
+      return this.select(0);
+    })(function (error, res) {
+      return this.smembers(key);
+    })(function (error, res) {
+      const payloads =[];
+      res.forEach((element) => {
+        payloads.push({ topic: element, messages: [JSON.stringify(msg)] });
+      }, this);
+      producer.send(payloads, (err, data) => {
+        console.log(data);
+      });
+    });
+  });
 });
-
-
 
