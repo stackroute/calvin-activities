22bc568ebf9e1ab4ce46dfd803b35ce0fd0f9a5b
ma301895 <mayank.sethi1@wipro.com>
2017-07-05 17:27:08 +0530

    fixed lint errors in kafka

--- kafka/index.js
+++ kafka/index.js
@@ -2,7 +2,7 @@ const config = require('./config.json');
 
 const kafka = require('kafka-node');
 
-const client = new kafka.Client(config.connectionString,config.clientId);
+const client = new kafka.Client(config.connectionString, config.clientId);
 
 const topics = config.topics;
 
@@ -10,13 +10,13 @@ const options = config.options;
 
 const Consumer = kafka.Consumer;
 
-const consumer = new Consumer(client,[topics],options);
+const consumer = new Consumer(client, [topics], options);
 
 const Producer = kafka.Producer;
 
 const producer = new Producer(client);
 
 module.exports = {
-    consumer,
-    producer
-}
+  consumer,
+  producer,
+};
