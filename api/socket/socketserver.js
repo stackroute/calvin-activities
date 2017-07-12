9b8682981f6804e0963ac8d8e42f12d8254a6b66
nithie <nithi.kbe@gmail.com>
2017-07-12 10:59:21 +0530

    Implemented push notification

--- api/socket/socketserver.js
+++ api/socket/socketserver.js
@@ -1,5 +1,6 @@
 const activityDAO = require('../../dao').activity;
 const authorize = require('../../authorize');
+const subscriber = require("redis").createClient([{host:'172.23.238.134', port:'6379'}]);
 
 function bootstrapSocketServer(io) {
   io.on('connection', (socket) => {
@@ -7,10 +8,14 @@ function bootstrapSocketServer(io) {
       activityDAO.publishToMailbox(data.mid, data.message, (err, result) => {});
     });
 
-    socket.on('authorize', (auth) => {
+  subscriber.on('message',function(channel, data){
+    activityDAO.publishActivityToListeners(channel, data);
+  });
+  socket.on('authorize', (auth) => {
       if (authorize.verify(auth, 'mailbox:all')) {
         socket.on('startListeningToMailbox', (mid) => {
           activityDAO.addListnerToMailbox(mid, socket);
+          subscriber.subscribe(mid);
         });
 
         socket.on('stopListeningToMailbox', (mid) => {
--- multiplexerDelivery/index.js
+++ multiplexerDelivery/index.js
@@ -2,7 +2,8 @@
 
 const kafkaClient = require('../client/kafkaclient');
 
-const redisClient = require('../client/redisclient');
+// const redisClient = require('../client/redisclient');
+const publisher = require("redis").createClient([{host:'172.23.238.134', port:'6379'}]);
 
 const topic =require('../config').kafka.topics.topic;
 
@@ -23,6 +24,7 @@ consumer.on('message', (message) => {
     if (!mailboxExists) { ({ message: 'Mailbox Id does not exists' }); return; }
     activityDao.publishToMailbox(receiver, newActivity, (error1, data1) => {
       if (error1) { ({ message: `${error1}` }); }
+      publisher.publish(receiver, JSON.stringify(newActivity));
     });
   });
 });
