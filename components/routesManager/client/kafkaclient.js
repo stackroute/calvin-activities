3afbb4fd994864ea967cce305221cd3f2f4a8408
priyangaDharmarajan <priyangadharmaraj.94@gmail.com>
2017-07-14 23:17:23 +0530

    eventprocessor and routesmanager are added

---
+++ components/routesManager/client/kafkaclient.js
@@ -0,0 +1,22 @@
+const config =require('../config').kafka;
+
+const kafka = require('kafka-node');
+
+const client = new kafka.Client(`${config.host}:${config.port}`);
+
+const topics = config.topics;
+
+const options = config.options;
+
+const Consumer = kafka.Consumer;
+
+const consumer = new Consumer(client, [topics], options);
+
+const Producer = kafka.Producer;
+
+const producer = new Producer(client);
+
+module.exports = {
+  consumer,
+  producer,
+};
