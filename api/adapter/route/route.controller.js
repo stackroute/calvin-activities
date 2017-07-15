de939bac8b0a6b67f53cf9611f0c10fc16355327
Gayathri-gayu <gp124120@gmail.com>
2017-07-15 18:52:53 +0530

    added route for domain in adapter

--- api/adapter/route/route.controller.js
+++ api/adapter/route/route.controller.js
@@ -1,7 +1,7 @@
 const client = require('../../../client/kafkaclient').client;
 const eventService = require('../../../services/event');
 
-function checkStatus(req, res) {
+function checkStatusForUser(req, res) {
   const event = {
     user: req.params.user,
     status: req.params.status,
@@ -9,7 +9,17 @@ function checkStatus(req, res) {
   eventService.sendevent(event);
   res.status(201).json(event);
 }
+
+function checkStatusForCircle(req, res) {
+  const event = {
+    domain: req.params.domain,
+    status: req.params.status,
+  }
+  eventService.sendevent(event);
+  res.status(201).json(event);
+
+}
 module.exports = {
-  checkStatus,
+  checkStatusForUser, checkStatusForCircle,
 };
 
