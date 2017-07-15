467db4c03fb68231ac5fc1fa3405a5570d694522
Gayathri-gayu <gp124120@gmail.com>
2017-07-15 12:32:07 +0530

    added adapter to mock DAO

--- api/adapter/follow/follow.controller.js
+++ api/adapter/follow/follow.controller.js
@@ -2,68 +2,67 @@ const adapterDAO = require('../../../dao/cassandra/adapter');
 const followDAO = require('../../../dao/cassandra/follow');
 
 function addFollow(req, res) {
-    adapterDAO.checkIfDomainExists(req.params.domain, (error, doesDomainExists) => {
+  adapterDAO.checkIfDomainExists(req.params.domain, (error, doesDomainExists) => {
     if (error) { res.status(500).json({ message: `${error}` }); return; }
     if (!doesDomainExists) {
       res.status(404).json({ message: 'Domain does not exist' });
       return;
     }
     adapterDAO.checkIfUserExists(req.params.user, (error, doesUserExists) => {
-    if (error) { res.status(500).json({ message: `${error}` }); return; }
-    if (!doesUserExists) {
-      res.status(404).json({ message: 'User does not exist' });
-      return;
-    }
-    const obj = {
+      if (error) { res.status(500).json({ message: `${error}` }); return; }
+      if (!doesUserExists) {
+        res.status(404).json({ message: 'User does not exist' });
+        return;
+      }
+      const obj = {
         // domain: req.params.domain,
         // user: req.params.user,
-        circleId:(doesDomainExists.circleid).toString(),
-        mailboxId:(doesUserExists.mailboxid).toString(),
-     }
-     const startFollowing = new Date();  
-  followDAO.addFollow(obj, startFollowing,(error,addfollower)=>{
-     if (error) { res.status(500).json({ message: `${error}` }); return; }
-       res.status(201).json(obj);
+        circleId: (doesDomainExists.circleid).toString(),
+        mailboxId: (doesUserExists.mailboxid).toString(),
+      }
+      const startFollowing = new Date();
+      followDAO.addFollow(obj, startFollowing, (error, addfollower) => {
+        if (error) { res.status(500).json({ message: `${error}` }); return; }
+        res.status(201).json(obj);
+      });
+    });
   });
-});
-});
 }
 
 
-function deleteFollow(req, res){
+function deleteFollow(req, res) {
 
-    adapterDAO.checkIfDomainExists(req.params.domain, (error, doesDomainExists) => {
+  adapterDAO.checkIfDomainExists(req.params.domain, (error, doesDomainExists) => {
     if (error) { res.status(500).json({ message: `${error}` }); return; }
     if (!doesDomainExists) {
       res.status(404).json({ message: 'Domain does not exist' });
       return;
     }
-    
+
     adapterDAO.checkIfUserExists(req.params.user, (error, doesUserExists) => {
-    if (error) { res.status(500).json({ message: `${error}` }); return; }
-    if (!doesUserExists) {
-      res.status(404).json({ message: 'User does not exist' });
-      return;
-    }
+      if (error) { res.status(500).json({ message: `${error}` }); return; }
+      if (!doesUserExists) {
+        res.status(404).json({ message: 'User does not exist' });
+        return;
+      }
 
-    const obj = {
-        circleId:(doesDomainExists.circleid).toString(),
-        mailboxId:(doesUserExists.mailboxid).toString(),
-     }
-    followDAO.checkIfFollowExists(obj,(error,doesFollowExists)=>{
+      const obj = {
+        circleId: (doesDomainExists.circleid).toString(),
+        mailboxId: (doesUserExists.mailboxid).toString(),
+      }
+      followDAO.checkIfFollowExists(obj, (error, doesFollowExists) => {
         if (error) { res.status(500).json({ message: `${error}` }); return; }
-            const isFollowExists = doesFollowExists;
+        const isFollowExists = doesFollowExists;
 
-        followDAO.deleteFollow( {circleId, mailboxId }, (err, result) => {
-          if (err) { res.status(500).json({ message: ` ${err}` });return;  }
+        followDAO.deleteFollow({ circleId, mailboxId }, (err, result) => {
+          if (err) { res.status(500).json({ message: ` ${err}` }); return; }
           res.status(200).json({ message: 'follow deleted' });
 
+        });
+      });
     });
-});
-});
-    });
+  });
 }
-    
+
 
 module.exports = { addFollow, deleteFollow, }
-   
