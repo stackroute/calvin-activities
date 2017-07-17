65ecc619f05d5d4b5e1d7357d3bdfbac6e58cfed
Gayathri-gayu <gp124120@gmail.com>
2017-07-17 16:54:09 +0530

    sync is not yet complete

--- components/event_processor/dao/getCircles.js
+++ components/event_processor/dao/getCircles.js
@@ -1,20 +1,54 @@
-const start=require('../client/dse');
+const start = require('../client/dse');
 
-const client=start.client;
+const client = start.client;
 
 function getCirclesForMailbox(mailboxId, callback) {
   const query = (`SELECT  circleid from circlesfollowedbymailbox where mailboxId = ${mailboxId}`);
   client.execute(query, (err, result) => {
     if (err) { throw err; }
-    // console.log(result);
     return callback(err, result);
   });
 }
-function getMailboxIdForCircle(circleId,callback){
+function getMailboxIdForCircle(circleId, callback) {
   const query = (`SELECT mailboxid from circle where circleid = ${circleId}`);
-  client.execute(query,(err,result) => {
-    if(err) {throw err ;}
-    return callback(err,result);
+  client.execute(query, (err, result) => {
+    if (err) { throw err; }
+    return callback(err, result);
   })
 }
-module.exports = { getCirclesForMailbox,getMailboxIdForCircle };
+
+function getLastMessageOfMailbox(mailboxId, callback) {
+  const query = (`SELECT payload from activity where mailboxid = ${mailboxId}`);
+  client.execute(query, (err, result) => {
+    if (err) { throw err; }
+    return callback(err, result);
+  })
+}
+
+const mailboxId = '81b0055e-969d-4a0e-a9ad-21a9ded9c550';
+const circlesMailboxId = [];
+getLastMessageOfMailbox(mailboxId, (err, result) => {
+  if (err) { return { message: 'err' } }
+  //else { console.log(result.rows); }
+})
+
+getCirclesForMailbox(mailboxId, (err, result) => {
+  if (err) { return { message: 'err' }; }
+  const rows = result.rows;
+  rows.forEach((x) => {
+    const circles = x.circleid.toString();
+    getMailboxIdForCircle(circles, (err, result) => {
+      if (err) { return { message: 'err' }; }
+      const rows = result.rows;
+      rows.forEach((x) => {
+        const mailboxes = x.mailboxid.toString();
+        getLastMessageOfMailbox(mailboxes, (err, result) => {
+          if (err) { return { message: 'err' } }
+          else { console.log(result.rows); }
+        })
+      });
+    });
+  });
+});
+
+module.exports = { getCirclesForMailbox, getMailboxIdForCircle, getLastMessageOfMailbox };
