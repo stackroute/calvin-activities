bc35314d4442af30253747d2dfb179db2cd1a8ca
nithie <nithi.kbe@gmail.com>
2017-07-18 15:32:18 +0530

    Modified socket server to accept parameter as object

--- components/rest-api/app.js
+++ components/rest-api/app.js
@@ -12,20 +12,26 @@ app.use(require('body-parser').json());
 
 const authorize = require('./authorize');
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   next();
+ });
+
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/circle', authorize.verifyToken, require('./api/circle'));
 
-app.use('/mailbox', authorize.verifyToken, require('./api/mailbox'));
+app.use('/mailbox', require('./api/mailbox'));
 
 /** Follow URI is for /mailbox/:mailboxId/circle/:circleId */
-app.use('/mailbox/', authorize.verifyToken, require('./api/follow'));
+app.use('/mailbox/', require('./api/follow'));
 
 // Publish activity to mailbox
-app.use('/mailbox', authorize.verifyToken, require('./api/activity'));
+app.use('/mailbox', require('./api/activity'));
 
 // Publish activity to circle
-app.use('/circle', authorize.verifyToken, require('./api/activity'));
+app.use('/circle', require('./api/activity'));
 
 // Multiplexer
 app.use('/multiplexer', require('./api/multiplexer'));
