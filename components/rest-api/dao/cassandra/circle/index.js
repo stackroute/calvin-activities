1666ef62f1a33fb1d21f51b8359279f8e4a5ff11
Himani <himani.singla@niit.com>
2017-07-20 14:56:39 +0530

    fixed rest-api segmentation fault

--- components/rest-api/dao/cassandra/circle/index.js
+++ components/rest-api/dao/cassandra/circle/index.js
@@ -23,10 +23,12 @@ function createCircle(callback) {
     client.execute(query, [newCircle.circleId, newCircle.mailboxId, newCircle.createdOn], (err, result) => {
       if (err) { console.log('ERR:', err); return callback(err, null); }
       console.log('Executed Query Successfully');
-      kafkaPipeline.producer.send([{topic: config.kafka.routesTopic, messages: JSON.stringify({circleId: newCircle.circleId, mailboxId: newCircle.mailboxId, command: 'addRoute'})}], (err, data) => {
+      kafkaPipeline.producer.ready(function() {
+        kafkaPipeline.producer.send([{topic: config.kafka.routesTopic, messages: JSON.stringify({circleId: newCircle.circleId, mailboxId: newCircle.mailboxId, command: 'addRoute'})}], (err, data) => {
         if(err){console.log('ERR:', err); return callback('Error while adding Circle route, messages published to this circle might be loosed'); }
         console.log('Produced');
         return callback(err, newCircle);
+      });
       });
     });
   });
