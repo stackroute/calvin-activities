c4ad605f3519d4bb64fd8e2adb0cbe7239045552
Sagar Patke <sagarpatke@gmail.com>
2017-07-21 09:24:16 +0530

    Removed Duplication of messages in producer

--- components/bootstrap.sh
+++ components/bootstrap.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-/opt/kafka/bin/kafka-topics.sh --create --zookeeper 172.23.238.205:2181 --replication-factor 1 --partitions 5 --topic m1
-/opt/kafka/bin/kafka-topics.sh --create --zookeeper 172.23.238.205:2181 --replication-factor 1 --partitions 5 --topic m1D
-/opt/kafka/bin/kafka-topics.sh --create --zookeeper 172.23.238.205:2181 --replication-factor 1 --partitions 5 --topic m2
-/opt/kafka/bin/kafka-topics.sh --create --zookeeper 172.23.238.205:2181 --replication-factor 1 --partitions 5 --topic m2D
-/opt/kafka/bin/kafka-topics.sh --create --zookeeper 172.23.238.205:2181 --replication-factor 1 --partitions 5 --topic m3
-/opt/kafka/bin/kafka-topics.sh --create --zookeeper 172.23.238.205:2181 --replication-factor 1 --partitions 5 --topic m3D
-/opt/kafka/bin/kafka-topics.sh --create --zookeeper 172.23.238.205:2181 --replication-factor 1 --partitions 5 --topic as_demo_activities
-/opt/kafka/bin/kafka-topics.sh --create --zookeeper 172.23.238.205:2181 --replication-factor 1 --partitions 5 --topic as_demo_routes
-/opt/kafka/bin/kafka-topics.sh --create --zookeeper 172.23.238.205:2181 --replication-factor 1 --partitions 5 --topic as_demo_events
+docker exec -it components_kafka_1 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 5 --topic m1
+docker exec -it components_kafka_1 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 5 --topic m1D
